72e1500cb8dc1b536a3a3619c83b20386a874301
kilati <kilati74@gmail.com>
2024-12-13 15:35:09 +0100

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
-import './index.css'
-// @deno-types="@types/react"
+import "./index.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
+import "react-circular-progressbar/dist/styles.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import { StrictMode } from 'react'
-// @deno-types="@types/react-dom/client"
+import App from "./App.tsx";
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
+    </StrictMode>,
+);
